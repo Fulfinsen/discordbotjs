e881ccbc4cc981a15a61cc6d3b13400cb2c1b56a
Fulfinsen <dincasilviu2@gmail.com>
2022-09-06 19:25:52 +0300

    DiscordBotJS

---
+++ DiscordBotJS/deploy-commands.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+	new SlashCommandBuilder().setName('avaible').setDescription('Is this server alive or dead'),
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('id').setDescription('Shows the ID'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+	.catch(console.error);
